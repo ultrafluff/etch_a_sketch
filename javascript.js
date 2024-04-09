6f5f57fed398dae0e65e03684a643b86d1713b90
ultrafluff <ultrafluffzander@gmail.com>
2024-04-09 13:45:22 -0400

    added the number of boxes depending on the number you choose

--- javascript.js
+++ javascript.js
@@ -10,14 +10,7 @@ function changecolor() {
 
 
 
-for (let count = 0; count < 256; count++) {
-    const div = document.createElement("div")
-    div.classList.add("box")
-    container.appendChild(div)
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "darkgray";
-    })
-}
+
 
 
 function gridpopup() {
@@ -29,17 +22,33 @@ function gridpopup() {
         alert("please choose a smaller grid")
         return
     }
-    if (num == 15) {
-        for (let count = 0; count < 31; count++) {
-            const div = document.querySelector(".box")
-            container.removeChild(div)
-        }
+    let boxnumber = num * num
+    
+    for (let count = 0; count < boxnumber; count++) {
+        const div = document.createElement("div")
+        div.classList.add("box")
+        container.appendChild(div)
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "darkgray";
+        })
     }
+
+
 }
+
+
+
+
+
+
 
 const button = document.querySelector("#btn")
 
 button.addEventListener("click", gridpopup)
+
+
+
+
 
 
 
